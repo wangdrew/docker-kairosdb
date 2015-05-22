65cd5a1971bec4395104855926edc38f1ede26e6
Emmanuel Frécon <emmanuel@sics.se>
2015-05-22 13:55:47 +0200

    Adding loglevel control

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN cd /opt; \
   curl -L https://github.com/kairosdb/kairosdb/releases/download/v0.9.4/kairosdb-0.9.4-6.tar.gz | \
   tar zxfp -
 
+ADD logback.xml /opt/kairosdb/conf/logging/logback.xml
 ADD kairosdb.properties /opt/kairosdb/conf/kairosdb.properties
 
 # Kairos API telnet and jetty ports
@@ -14,6 +15,7 @@ EXPOSE 4242 8083
 # Set Kairos config vars
 #ENV KAIROS_JETTY_PORT 8083
 ENV CASSANDRA_HOST_LIST 10.1.2.3:9160
+ENV LOGLEVEL DEBUG
 
 # Copy scripts into container to set kairos config params
 ADD config-kairos.sh /usr/bin/config-kairos.sh
--- config-kairos.sh
+++ config-kairos.sh
@@ -3,11 +3,16 @@
 if [[ -z "$CASSANDRA_HOST_LIST" ]]; then
     export CASSANDRA_HOST_LIST=10.1.2.3:9160
 fi
+if [[ -z "$LOGLEVEL" ]]; then
+    export LOGLEVEL=INFO
+fi
 #if [[ -z "$KAIROS_JETTY_PORT" ]]; then
 #    export KAIROS_JETTY_PORT=8083
 #fi
 
+export JAVA_OPTS=-Dlogback.configurationFile=/opt/kairosdb/conf/logging/logback.xml
 #sed -i "s/^kairosdb.jetty.port.*$/kairosdb.jetty.port=$KAIROS_JETTY_PORT/" /opt/kairosdb/conf/kairosdb.properties
 sed -i "s/^kairosdb.datastore.cassandra.host_list.*$/kairosdb.datastore.cassandra.host_list=$CASSANDRA_HOST_LIST/" /opt/kairosdb/conf/kairosdb.properties
+sed -ri "s/level=\"[A-Z]+\"/level=\"$LOGLEVEL\"/g" /opt/kairosdb/conf/logging/logback.xml
 
 /opt/kairosdb/bin/kairosdb.sh run
